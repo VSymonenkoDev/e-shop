04a9668cebdb63dcc2b4cb6b653991e688a5ad55
VSymonenkoDev <v.simonenko@mobilunity.com>
2018-09-19 18:27:26 +0300

    nav bar fix

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {LoginComponent} from './components/login/login.component';
 import {BootstrapNavbarComponent} from './components/bootstrap-navbar/bootstrap-navbar.component';
 import {HomeComponent} from './components/home/home.component';
 import {RouterModule} from '@angular/router';
+import {SharedModule} from 'shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     RouterModule.forChild([])
   ],
   declarations: [
